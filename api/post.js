6f2e4e4c4028c21f703a23d686d4804fc835bcac
vivekkumarsingh103 <bibegdemon12@gmail.com>
2025-12-02 18:18:00 +0530

    Update post.js

--- api/post.js
+++ api/post.js
@@ -1,1 +1,13 @@
 
+{
+  "name": "dramawallah-bot",
+  "version": "1.0.0",
+  "description": "Telegram bot with image upload",
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "dependencies": {
+    "mongodb": "^6.3.0",
+    "cloudinary": "^1.41.1"
+  }
+}
